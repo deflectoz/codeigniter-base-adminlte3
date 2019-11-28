e404170c6e56e16b7959f657dec5a3d7aaffca7b
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2019-11-28 10:51:11 +0700

    first commit

---
+++ application/views/layout/footer.php
@@ -0,0 +1,26 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.2-pre
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?= base_url() ?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?= base_url() ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="<?= base_url() ?>dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="<?= base_url() ?>plugins/chart.js/Chart.min.js"></script>
+<script src="<?= base_url() ?>dist/js/demo.js"></script>
+<script src="<?= base_url() ?>dist/js/pages/dashboard3.js"></script>
+</body>
+</html>
